7d66e14fbba50a125efa9ae36e89a3e20695004f
Noah-Waisbrod <20nsw@queensu.ca>
2023-02-27 13:34:28 -0500

    Update Main.ino

--- Main Project/Main/Main.ino
+++ Main Project/Main/Main.ino
@@ -26,20 +26,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(SafteyDistance(900.0,900.0));
+  delay(1000);
   
 }
 
 //---------------Functions------------------
+
+//PID Motor
+void PID(){
+  
+  }
+
 //function to check safty distance
 boolean SafteyDistance(double frontDist, double sideDist){
     double FD = map(analogRead(sharpF),0,1023,0,3300);
-    double LD = map(analogRead(sharpF),0,1023,0,3300);
-    double RD = map(analogRead(sharpF),0,1023,0,3300);
-    if(FD <= frontDist || LD <= sideDist || RD <= sideDist){
+    double LD = map(analogRead(sharpL),0,1023,0,3300);
+    double RD = map(analogRead(sharpR),0,1023,0,3300);
+    if(FD <= frontDist && LD <= sideDist && RD <= sideDist){
        return true; 
     } else {
-        return false;
+       return false;
     }
 }
-
-
