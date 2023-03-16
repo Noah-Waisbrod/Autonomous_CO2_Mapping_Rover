6f2fccb81fbfa7fa90e74972a113dbf998ce44ad
Noah-Waisbrod <20nsw@queensu.ca>
2023-03-16 16:03:48 -0400

    Update Main.ino

--- Main Project/Main/Main.ino
+++ Main Project/Main/Main.ino
@@ -23,66 +23,8 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGBW + NEO_KHZ8
 //-CO2-
 Adafruit_SGP30 sgp;
 
-
 //-wheels-
-// Wheel PWM pin (must be a PWM pin)
-int EA = 6; // Right wheel
-int EB = 5; // Left wheel
 
-// Wheel direction digital pins
-int I1 = 8; // Forwards (R)
-int I2 = 7; // Backwards (R)
-
-int I3 = 13; // Backwards (L)
-int I4 = 12; // Forwards (L)
-
-int LF = 100;
-int LB = 200;
-int RF = 300;
-int RB = 400;
-
-// Motor PWM command variable [0-255]
-byte uL = 0;
-byte uR = 0;
-
-// Left wheel encoder digital pins
-const byte SIGNAL_A_L = 3;
-const byte SIGNAL_B_L = 2;
-
-const byte SIGNAL_A_R = 9;
-const byte SIGNAL_B_R = 10;
-
-// Encoder ticks per (motor) revolution (TPR)
-const int TPR = 3000;
-
-// Wheel radius [m]
-const double RHO = 0.0625;
-
-// Counter to keep track of encoder ticks [integer]
-volatile long encoder_ticks_L = 0;
-volatile long encoder_ticks_R = 0;
-
-// Variable to store estimated angular rate of left wheel [rad/s]
-double omega_L = 0.0;
-double omega_R = 0.0;
-
-// Sampling interval for measurements in milliseconds
-const int T = 70;
-
-// Counters for milliseconds during interval
-long t_now = 0;
-long t_last = 0;
-
-#define MAX_PWM 255
-#define MIN_PWM 0
-
-double kp = 250;
-double ki = 50;
-double kd = 0.0;
-double error_L, p_error_L, i_error_L, d_error_L, error_R, p_error_R, i_error_R, d_error_R;
-double pwm_L, pwm_R;
-double setpoint = 0.70; // m/s
-double actual_speed_L,actual_speed_R;
 
 //-----------------------------------Setup-------------------------------
 void setup() {
@@ -115,14 +57,7 @@ void setup() {
 
 //----------------------------------main--------------------------------
 void loop() {
-  if (! sgp.IAQmeasure()) {
-    Serial.println("Measurement failed");
-    return;
-  }
-  Serial.print("CO2: ");
-  Serial.print(getCO2());
-  Serial.print(" ppm");
-  Serial.println("");
+  
 
   delay(1000);
   
@@ -132,7 +67,20 @@ void loop() {
 
 //Function to get the CO2 reading
 int getCO2(){
+  if (! sgp.IAQmeasure()) {
+    Serial.println("Measurement failed");
+    return;
+  }
   return sgp.eCO2;
+  }
+
+//Function to get the TVOC (Total Voletile Oganic Comounds) reading
+int getTVOC(){
+  if (! sgp.IAQmeasure()) {
+    Serial.println("Measurement failed");
+    return;
+  }
+  return sgp.TVOC;
   }
 
 //PID Motor
