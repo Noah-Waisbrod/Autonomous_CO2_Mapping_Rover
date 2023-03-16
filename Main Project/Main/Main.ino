1de9f33f9deef7765e842638b48400e7cf05723f
Noah-Waisbrod <20nsw@queensu.ca>
2023-03-16 17:44:05 -0400

    Update Main.ino

--- Main Project/Main/Main.ino
+++ Main Project/Main/Main.ino
@@ -2,6 +2,7 @@
 //-------------------------------------
 //Names          : Noah Waisbrod, Mustafa Hasan, Matthew Murray
 //Student Numbers: 20274009
+//Sources        : Thomas Sears (thomas.sears@queensu.ca)(PI-Controller)
 //------------------------------------- 
 
 //---------------------------------Imports----------------------------------
@@ -10,21 +11,98 @@
 #include <Adafruit_SGP30.h>
 //------------------------------pin assignments-----------------------------
 
-//-SHARPS-
+//----SHARPS----
 int sharpF = A5;
 int sharpL = A4;
 int sharpR = A3;
 
-//-LED-
-int PIN = 6;
+//----LED----
+int PIN = 2;
 int NUMPIXELS = 8;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGBW + NEO_KHZ800);
 
-//-CO2-
+//----CO2----
 Adafruit_SGP30 sgp;
 
-//-wheels-
+//----wheels----
+int EB = 5; // Wheel PWM pin (must be a PWM pin)
+int I3 = 3; // Wheel direction digital pin 1
+int I4 = 4; // Wheel direction digital pin 2
+int EA = 6; // Wheel PWM pin (must be a PWM pin)
+int I1 = 8; // Wheel direction digital pin 1
+int I2 = 7; // Wheel direction digital pin 2
+const byte SIGNAL_AL = 11; // green wire
+const byte SIGNAL_BL = 12; // yellow wire
+const byte SIGNAL_AR = 9;  // green wire
+const byte SIGNAL_BR = 10; // yellow wire
 
+const int TPR = 3000;         // Encoder ticks per (motor) revolution (TPR)
+const double RHO = 0.0625;    // Wheel radius [m]
+const double ELL = 0.2775;    // Vehicle track [m]
+const int T = 100;            // Sampling interval for measurements in milliseconds
+const double KP = 200.0;      // Proportional gain
+const double KI = 100.0;      // Integral gain
+
+// Motor PWM command variables [0-255]
+short u_L = 0;
+short u_R = 0;
+// Counter to keep track of encoder ticks [integer]
+volatile long encoder_ticks_L = 0;
+volatile long encoder_ticks_R = 0;
+// Variables to store estimated angular rates of wheels [rad/s]
+double omega_L = 0.0;
+double omega_R = 0.0;
+// Variables to store estimated wheel speeds [m/s]
+double v_L = 0.0;
+double v_R = 0.0;
+// Variables to store vehicle speed and turning rate
+double v = 0.0;     // [m/s]
+double omega = 0.0; // [rad/s]
+// Variables to store desired vehicle speed and turning rate
+double v_d = 0.0;     // [m/s]
+double omega_d = 0.0; // [rad/s]
+// Variable to store desired wheel speeds [m/s]
+double v_Ld = 0.0;
+double v_Rd = 0.0;
+// Counters for milliseconds during interval
+long t_now = 0;
+long t_last = 0;
+// Variables to store errors for controller
+double e_L = 0.0;
+double e_R = 0.0;
+double e_Lint = 0.0;
+double e_Rint = 0.0;
+
+//-----helper function-------
+// This function is called when SIGNAL_AL (left encoder) goes HIGH
+void decodeEncoderTicks_L()
+{
+    if (digitalRead(SIGNAL_BL) == LOW)
+    {
+        // SIGNAL_A leads SIGNAL_B, so count one way
+        encoder_ticks_L--;
+    }
+    else
+    {
+        // SIGNAL_B leads SIGNAL_A, so count the other way
+        encoder_ticks_L++;
+    }
+}
+
+// This function is called when SIGNAL_AR (right encoder) goes HIGH
+void decodeEncoderTicks_R()
+{
+    if (digitalRead(SIGNAL_BR) == LOW)
+    {
+        // SIGNAL_A leads SIGNAL_B, so count one way
+        encoder_ticks_R++;
+    }
+    else
+    {
+        // SIGNAL_B leads SIGNAL_A, so count the other way
+        encoder_ticks_R--;
+    }
+}
 
 //-----------------------------------Setup-------------------------------
 void setup() {
@@ -51,6 +129,38 @@ void setup() {
       Serial.print(".");
       delay(1000);
     }
+
+    //PID
+    // Configure digital pins for output
+    pinMode(EA, OUTPUT);
+    pinMode(I1, OUTPUT);
+    pinMode(I2, OUTPUT);
+
+    // Configure digital pins for output
+    pinMode(EB, OUTPUT);
+    pinMode(I3, OUTPUT);
+    pinMode(I4, OUTPUT);
+
+    // Set the pin modes for the encoders
+    pinMode(SIGNAL_AL, INPUT);
+    pinMode(SIGNAL_BL, INPUT);
+    pinMode(SIGNAL_AR, INPUT);
+    pinMode(SIGNAL_BR, INPUT);
+
+    // Send 0 PWM commands
+    analogWrite(EA, 0);
+    analogWrite(EB, 0);
+
+    // Send brake signals to motor driver
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, LOW);
+    digitalWrite(I3, LOW);
+    digitalWrite(I4, LOW);
+
+    // Every time the pin goes high, this is a pulse; enable the interrupts
+    attachInterrupt(digitalPinToInterrupt(SIGNAL_AL), decodeEncoderTicks_L, RISING);
+    attachInterrupt(digitalPinToInterrupt(SIGNAL_AR), decodeEncoderTicks_R, RISING);
+    
     //Start 
     Serial.println("Program ready.");
 }
@@ -58,13 +168,13 @@ void setup() {
 //----------------------------------main--------------------------------
 void loop() {
   
-
-  delay(1000);
+  PIControler(1,0);
   
 }
 
 //-------------------------------Functions------------------------------
 
+//-----------CO2-----------
 //Function to get the CO2 reading
 int getCO2(){
   if (! sgp.IAQmeasure()) {
@@ -83,16 +193,19 @@ int getTVOC(){
   return sgp.TVOC;
   }
 
-//PID Motor
-void PID(){
-  
-  }
-
+//-----------SHARP-----------
 //function to check safty distance
 boolean SafteyDistance(double frontDist, double sideDist){
-    double FD = map(analogRead(sharpF),0,1023,0,3300);
-    double LD = map(analogRead(sharpL),0,1023,0,3300);
-    double RD = map(analogRead(sharpR),0,1023,0,3300);
+      double FD = map(analogRead(sharpF),0,1023,0,3300);
+      double LD = map(analogRead(sharpL),0,1023,0,3300);
+      double RD = map(analogRead(sharpR),0,1023,0,3300);
+    
+    for(int i = 0; i < 10; i++){
+      FD = (FD + map(analogRead(sharpF),0,1023,0,3300))/2;
+      LD = (LD + map(analogRead(sharpL),0,1023,0,3300))/2;
+      RD = (RD + map(analogRead(sharpR),0,1023,0,3300))/2;
+      delay(10);
+    }
     if(FD <= frontDist && LD <= sideDist && RD <= sideDist){
        return true; 
     } else {
@@ -100,6 +213,7 @@ boolean SafteyDistance(double frontDist, double sideDist){
     }
 }
 
+//-----------LED-----------
 //Function to update the LED stick
 // 0 = OFF
 // 1 = GREEN
@@ -118,4 +232,171 @@ void setNeoPixelColor(int colour) {
             }
     }
   pixels.show(); // update the NeoPixel stick
+}
+
+//-------------------------------PID------------------------------
+//-----------Helper Functions-----------
+// This function applies PWM inputs (u_L and u_R) to the right and left wheels
+void driveVehicle(short u_L, short u_R)
+{
+    // LEFT WHEEL
+    if (u_L < 0) // If the controller calculated a negative input...
+    {
+        digitalWrite(I3, HIGH); // Drive backward (left wheels)
+        digitalWrite(I4, LOW);  // Drive backward (left wheels)
+
+        analogWrite(EB, -u_L); // Write left motors command
+    }
+    else // the controller calculated a positive input
+    {
+        digitalWrite(I3, LOW);  // Drive forward (left wheels)
+        digitalWrite(I4, HIGH); // Drive forward (left wheels)
+
+        analogWrite(EB, u_L); // Write left motors command
+    }
+
+    // RIGHT WHEEL
+    if (u_R < 0) // If the controller calculated a negative input...
+    {
+        digitalWrite(I1, LOW);  // Drive backward (right wheels)
+        digitalWrite(I2, HIGH); // Drive backward (right wheels)
+
+        analogWrite(EA, -u_R); // Write right motors command
+    }
+    else // the controller calculated a positive input
+    {
+        digitalWrite(I1, HIGH); // Drive forward (right wheels)
+        digitalWrite(I2, LOW);  // Drive forward (right wheels)
+
+        analogWrite(EA, u_R); // Write right motors command
+    }
+}
+
+// Compute the wheel rate from elapsed time and encoder ticks [rad/s]
+double compute_wheel_rate(long encoder_ticks, double delta_t)
+{
+    double omega;
+    omega = 2.0 * PI * ((double)encoder_ticks / (double)TPR) * 1000.0 / delta_t;
+    return omega;
+}
+
+// Compute wheel speed [m/s]
+double compute_wheel_speed(double omega_wheel)
+{
+    double v_wheel;
+    v_wheel = omega_wheel * RHO;
+    return v_wheel;
+}
+
+// Compute vehicle speed [m/s]
+double compute_vehicle_speed(double v_L, double v_R)
+{
+    double v;
+    v = 0.5 * (v_L + v_R);
+    return v;
+}
+
+// Compute vehicle turning rate [rad/s]
+double compute_vehicle_rate(double v_L, double v_R)
+{
+    double omega;
+    omega = 1.0 / ELL * (v_R - v_L);
+    return omega;
+}
+
+// Compute v_L from v and omega
+double compute_L_wheel_speed(double v, double omega)
+{
+    double v_wheel = 0.0;
+    v_wheel = v - ELL / 2.0 * omega;
+    return v_wheel;
+}
+
+// Compute v_R from v and omega
+double compute_R_wheel_speed(double v, double omega)
+{
+    double v_wheel = 0.0;
+    v_wheel = v + ELL / 2.0 * omega;
+    return v_wheel;
+}
+
+// Wheel speed PI controller function
+short PI_controller(double e_now, double e_int, double k_P, double k_I)
+{
+    short u;
+    u = (short)(k_P * e_now + k_I * e_int);
+
+    // Saturation (i.e., maximum input) detection
+    if (u > 255)
+    {
+        u = 255;
+    }
+    else if (u < -255)
+    {
+        u = -255;
+    }
+    return u;
+}
+
+
+//-------------control-------------
+void PIControler(double vd, double od){
+    // Get the elapsed time [ms]
+    t_now = millis();
+
+    // Perform control update every T milliseconds
+    if (t_now - t_last >= T)
+    {
+
+        // Set the desired vehicle speed and turning rate
+        v_d = 0.5;     // [m/s]
+        omega_d = 0.0; // [rad/s]
+
+        // Estimate the rotational speed of each wheel [rad/s]
+        omega_L = compute_wheel_rate(encoder_ticks_L, (double)(t_now - t_last));
+        omega_R = compute_wheel_rate(encoder_ticks_R, (double)(t_now - t_last));
+
+        // Compute the speed of each wheel [m/s]
+        v_L = compute_wheel_speed(omega_L);
+        v_R = compute_wheel_speed(omega_R);
+
+        // Compute the speed of the vehicle [m/s]
+        v = compute_vehicle_speed(v_L, v_R);
+
+        // Compute the turning rate of the vehicle [rad/s]
+        omega = compute_vehicle_rate(v_L, v_R);
+
+        // Record the current time [ms]
+        t_last = t_now;
+
+        // Reset the encoder ticks counter
+        encoder_ticks_L = 0;
+        encoder_ticks_R = 0;
+
+        // Compute the desired wheel speeds from v_d and omega_d
+        v_Ld = compute_L_wheel_speed(v_d, omega_d);
+        v_Rd = compute_R_wheel_speed(v_d, omega_d);
+
+        // Compute errors
+        e_L = v_Ld - v_L;
+        e_R = v_Rd - v_R;
+
+        // Integrate errors with anti-windup
+        if (abs(u_L) < 255)
+        {
+            e_Lint += e_L;
+        }
+        if (abs(u_R) < 255)
+        {
+            e_Rint += e_R;
+        }
+
+        // Compute control signals using PI controller
+        u_L = PI_controller(e_L, e_Lint, KP, KI);
+        u_R = PI_controller(e_R, e_Rint, KP, KI);
+
+        // Drive the vehicle
+        driveVehicle(u_L, u_R);
+
+    }
 }
